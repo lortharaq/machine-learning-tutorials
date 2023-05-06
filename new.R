db7a2271a64e6dd0c586c4d02b9b9a2f9152f03e
lortharaq <lortharaq@gmail.com>
2023-05-06 23:04:10 +0200

    testing

---
+++ new.R
@@ -0,0 +1,20 @@
+# Get current working directory
+getwd()
+
+# set working directory
+setwd("C:/Users/Lorthar Quansah/Documents/myproject/machine-learning-tutorials")
+
+#assigning a number to a variable
+x <- 7
+x
+
+2*x
+
+#assign character to a variable
+y <- "waae"
+y <- waae # this is error because waae is not a variable
+y <- x # reassign the value of x to y
+
+h <- 7 + rnorm(n=1, mean=0, sd=1)
+(10 + 2) * 5
+10 + 2 * 5
